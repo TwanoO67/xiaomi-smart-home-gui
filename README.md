64adb4b1ab1675f5a20557168a78bd1dc55df4a8
WEBER Antoine <ghostsmaker@hotmail.com>
2016-12-14 23:55:27 +0100

    nouveau format de base

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Xiaomi Smart Home - GUI
 
 construction du docker
-`docker build -t myuser/containername`
+
+```docker build -t myuser/containername```
+
 
 lancement du logger
-`docker run -v /mydiskdata/store:/mihome/db -d --net host --name xiaomi_logger myuser/containername`
+
+```docker run -v /mydiskdata/store:/mihome/db -d --net host --name xiaomi_logger myuser/containername```
+
 
 ## Principe de fonctionnement
 
 1) il faut mettre sa "Xiaomi Smart Home" en mode LAN, ainsi elle enverra les push Zigbee, en broadcast sur le réseau local
-2) le logger en nodejs capture les trames sur le réseau local (fork de homebridge-aquara) et les insères dans une base SQLite (un seul fichier simple a backupé)
-3) L'API donne accés depuis un service REST en php au contenu de la base SQLite (fork de php_crud_api)
-3) Le GUI en angular2 (fork de ng2-admin-lte) offre une visualisation de ces données (WAF friendly)
+2) le logger en nodejs capture les trames sur le réseau local et les insères dans une base SQLite (un seul fichier simple a backupé)
+3) L'API donne accés depuis un service REST en php au contenu de la base SQLite
+4) Le GUI en angular2 (fork de ng2-admin-lte) offre une visualisation de ces données (WAF friendly)
+
+## Source
+Logger: fork de [homebridge-aquara](https://github.com/snOOrz/homebridge-aqara)
+API: fork de [mevdschee/php-crud-api](https://github.com/mevdschee/php-crud-api) et de [alexpls/cart_rest_example](https://github.com/alexpls/cart_rest_example)
+GUI: fork de [TwanoO67/ng2-admin-lte](https://github.com/TwanoO67/ng2-admin-lte)
+Commande xiaomi https://louiszl.gitbooks.io/lumi-gateway-local-api/content/plug.html (CN)
+
+## Outils de debug
+
+Pour verifier le contenu de la DB SQLite, vous pouvez utiliser: http://sqlitebrowser.org/
--- logger/logger.js
+++ logger/logger.js
@@ -13,11 +13,83 @@ var db = new sqlite3.Database(path.join(__dirname, '..', 'db', 'database.db'));
 
 db.serialize(function() {
   db.run("CREATE TABLE IF NOT EXISTS xiaomi_log (id INTEGER PRIMARY KEY AUTOINCREMENT, date INTEGER, sid TEXT, model TEXT, cmd TEXT,data TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS xiaomi_device (sid TEXT PRIMARY KEY, name TEXT, model TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS xiaomi_heartbeat (id INTEGER PRIMARY KEY AUTOINCREMENT, sid TEXT, model TEXT, is_last_state INTEGER, data TEXT, interval_begin_date INTEGER, interval_end_date INTEGER, last_hb_date INTEGER)");
 });
 
-var stmt = db.prepare("INSERT INTO xiaomi_log (date,sid,model,cmd,data) VALUES (?,?,?,?,?)");
+var log_insert = db.prepare("INSERT INTO xiaomi_log (date,sid,model,cmd,data) VALUES (?,?,?,?,?)");
+var device_insert = db.prepare("INSERT OR IGNORE INTO xiaomi_device (sid,name,model) VALUES (?,?,?)");
+var device_update_model = db.prepare("UPDATE xiaomi_device SET model = ? WHERE sid = ?");
+var interval_begin = db.prepare("INSERT INTO xiaomi_heartbeat (sid,model,data,interval_begin_date,last_hb_date,last) VALUES (?,?,?,?,?,1)");
+var interval_update_hb = db.prepare("UPDATE xiaomi_heartbeat SET last_hb_date=? WHERE id=?");
+var interval_end = db.prepare("UPDATE xiaomi_heartbeat SET last_hb_date=?, interval_end_date=?, is_last_state=0 WHERE id=? ");
 //db.close();
 
+//interprete les log en fonctions du type
+function logDecode(json){
+  var model = json['model'];
+  var data = JSON.parse(json['data']);
+  if (model === 'sensor_ht') {
+    var temperature = data['temperature'] ? data['temperature'] / 100.0 : 100;
+    var humidity = data['humidity'] ? data['humidity'] / 100.0 : 0;
+    console.log("Step 7. Got temperature/humidity sensor:%s's heartbeat: temperature %d, humidity %d", json['sid'], temperature, humidity);
+  } else if (model === 'motion') {
+    console.log("Step 7. Got motion sensor:%s's heartbeat: move %s", json['sid'], (data['status'] === 'motion') ? 'detected' : 'not detected');
+  } else if (model === 'magnet') {
+    console.log("Step 7. Got contact/magnet sensor:%s's heartbeat: contact %s", json['sid'], (data['status'] === 'close') ? 'detected' : 'not detected');
+  } else if (model === 'ctrl_neutral1') {
+    console.log("Step 7. Got light switch:%s's heartbeat: %s", json['sid'], data['channel_0']);
+  } else if (model === 'ctrl_neutral2') {
+    console.log("Step 7. Got duplex light switch:%s's heartbeat: left %s, right %s", json['sid'], data['channel_0'], data['channel_1']);
+  } else if (model === 'gateway') {
+    console.log("Step 7. Got gateway:%s's heartbeat:%s with token:%s", json['sid'], json['data'], json['token']);
+  } else {
+    console.log("Step 7. Got %s:%s's heartbeat:%s", json['model'], json['sid'], json['data']);
+  }
+}
+
+function sendWhois() {
+  var cmd = '{"cmd": "whois"}';
+  serverSocket.send(cmd, 0, cmd.length, multicastPort, multicastAddress);
+  console.log('Step 2. Send %s to a multicast address %s:%d.', cmd, multicastAddress, multicastPort);
+}
+
+function updateInterval(json){
+  //ici on retire certain event, nottament les button (on ne veut pas suivre les periodes de non-appuie..)
+  if(json['model'] === "switch"){
+    return true;
+  }
+
+  db.all(" SELECT * from xiaomi_heartbeat WHERE sid = '"+json['sid']+"' AND is_last_state = 1 ", function(err, rows) {
+      if(err){
+        console.error(err);
+        exit;
+      }
+      //si aucune ligne
+      if(rows.length == 0){
+        //on crée un nouvel interval
+        interval_begin.run(json['sid'],json['model'],json['data'],Date.now(),Date.now());
+      }
+      //si on a deja une ligne
+      else{
+        //on verifie si data sont les memes
+        var row = rows[0];
+        if(row.data === json['data']){
+          //si oui on update la date de last_hb_date
+          interval_update_hb.run(Date.now(),row.id);
+        }
+        else{
+          //si non on ferme l'interv
+          interval_end.run(Date.now(),Date.now(),row.id);
+          //puis on crée un nouveau avec les new data
+          interval_begin.run(json['sid'],json['model'],json['data'],Date.now(),Date.now());
+        }
+      }
+  });
+}
+
+//exemple de messega recu:
+//{"cmd":"heartbeat","model":"gateway","sid":"f0b4299a63d9","short_id":"0","token":"1hX9gW20eIkqZRlY","data":"{\"ip\":\"192.168.0.12\"}"}
 serverSocket.on('message', function(msg, rinfo){
   console.log('recv %s(%d bytes) from client %s:%d\n', msg, msg.length, rinfo.address, rinfo.port);
   var json;
@@ -27,85 +99,59 @@ serverSocket.on('message', function(msg, rinfo){
     console.log('Unexpected message: %s', msg);
     return;
   }
-  stmt.run(Date.now(),json['sid'], model, json['cmd'], json['data']);
+
   var cmd = json['cmd'];
+  //reception de la reponse a "whois", chaque hub renvoi cela
   if (cmd === 'iam') {
     var address = json['ip'];
     var port = json['port'];
     var cmd = '{"cmd":"get_id_list"}';
+    device_insert.run(json['sid'],"Unknown Gateway","gateway");
     console.log('Step 3. Send %s to %s:%d', cmd, address, port);
     serverSocket.send(cmd, 0, cmd.length, port, address);
-  } else if (cmd === 'get_id_list_ack') {
+  }
+  //reception de la liste des devices d'un hub
+  else if (cmd === 'get_id_list_ack') {
     var data = JSON.parse(json['data']);
     for(var index in data) {
       var sid = data[index];
+      //on insere les nouvelles devices
+      device_insert.run(sid,"Unknown device");
+
+      //on demande a chaque device son etat
       var response = '{"cmd":"read", "sid":"' + sid + '"}';
-      // Remember this sid's address
+      // on stoque les infos de la gateway sur lequel contacté ce device sid
       sidToAddress[sid] = rinfo.address;
       sidToPort[sid] = rinfo.port;
 
       console.log('Step 4. Send %s to %s:%d', response, rinfo.address, rinfo.port);
       serverSocket.send(response, 0, response.length, rinfo.port, rinfo.address);
     }
-  } else if (cmd === 'read_ack') {
+  }
+  //on recoi l'etat d'une device
+  else if (cmd === 'read_ack') {
+    log_insert.run(Date.now(),json['sid'], json['model'], json['cmd'], json['data']);
+    device_update_model.run(json['model'],json['sid']);
+    updateInterval(json);
+    logDecode(json);
+  }
+  //on recoit une action push
+  else if (cmd === 'report') {
+    log_insert.run(Date.now(),json['sid'], json['model'], json['cmd'], json['data']);
+    //necessaire pour le motion (entre autre), mais a eviter pour le button
+    updateInterval(json);
+    //ici il faudra verifier si on doit executer des scenarios
+    logDecode(json);
+  }
+  //on recoit une action type ping
+  else if (cmd === 'heartbeat') {
     var model = json['model'];
     var data = JSON.parse(json['data']);
-
-    if (model === 'sensor_ht') {
-      var temperature = data['temperature'] ? data['temperature'] / 100.0 : 100;
-      var humidity = data['humidity'] ? data['humidity'] / 100.0 : 0;
-      console.log("Step 5. Got temperature/humidity sensor:%s's data: temperature %d, humidity %d", json['sid'], temperature, humidity);
-
-    } else if (model === 'motion') {
-      console.log("Step 5. Got motion sensor:%s's data: move %s", json['sid'], (data['status'] === 'motion') ? 'detected' : 'not detected');
-    } else if (model === 'magnet') {
-      console.log("Step 5. Got contact/magnet sensor:%s's data: contact %s", json['sid'], (data['status'] === 'close') ? 'detected' : 'not detected');
-    } else if (model === 'ctrl_neutral1') {
-      console.log("Step 5. Got light switch:%s's data: %s", json['sid'], data['channel_0']);
-    } else if (model === 'ctrl_neutral2') {
-      console.log("Step 5. Got duplex light switch:%s's data: left %s, right %s", json['sid'], data['channel_0'], data['channel_1']);
-    } else {
-      console.log("Step 5. Got %s:%s's data:%s", json['model'], json['sid'], json['data']);
-    }
-  } else if (cmd === 'report') {
-    var model = json['model'];
-    var data = JSON.parse(json['data']);
-    if (model === 'sensor_ht') {
-      var temperature = data['temperature'] ? data['temperature'] / 100.0 : 100;
-      var humidity = data['humidity'] ? data['humidity'] / 100.0 : 0;
-      console.log("Step 6. Got temperature/humidity sensor:%s's report: temperature %d, humidity %d", json['sid'], temperature, humidity);
-    } else if (model === 'motion') {
-      console.log("Step 6. Got motion sensor:%s's report: move %s", json['sid'], (data['status'] === 'motion') ? 'detected' : 'not detected');
-    } else if (model === 'magnet') {
-      console.log("Step 6. Got contact/magnet sensor:%s's report: contact %s", json['sid'], (data['status'] === 'close') ? 'detected' : 'not detected');
-    } else if (model === 'ctrl_neutral1') {
-      console.log("Step 6. Got light switch:%s's report: %s", json['sid'], data['channel_0']);
-    } else if (model === 'ctrl_neutral2') {
-      console.log("Step 6. Got duplex light switch:%s's report: left %s, right %s", json['sid'], data['channel_0'], data['channel_1']);
-    } else {
-      console.log("Step 6. Got %s:%s's report:%s", json['model'], json['sid'], json['data']);
-    }
-  } else if (cmd === 'heartbeat') {
-    var model = json['model'];
-    var data = JSON.parse(json['data']);
-    if (model === 'sensor_ht') {
-      var temperature = data['temperature'] ? data['temperature'] / 100.0 : 100;
-      var humidity = data['humidity'] ? data['humidity'] / 100.0 : 0;
-      console.log("Step 7. Got temperature/humidity sensor:%s's heartbeat: temperature %d, humidity %d", json['sid'], temperature, humidity);
-    } else if (model === 'motion') {
-      console.log("Step 7. Got motion sensor:%s's heartbeat: move %s", json['sid'], (data['status'] === 'motion') ? 'detected' : 'not detected');
-    } else if (model === 'magnet') {
-      console.log("Step 7. Got contact/magnet sensor:%s's heartbeat: contact %s", json['sid'], (data['status'] === 'close') ? 'detected' : 'not detected');
-    } else if (model === 'ctrl_neutral1') {
-      console.log("Step 7. Got light switch:%s's heartbeat: %s", json['sid'], data['channel_0']);
-    } else if (model === 'ctrl_neutral2') {
-      console.log("Step 7. Got duplex light switch:%s's heartbeat: left %s, right %s", json['sid'], data['channel_0'], data['channel_1']);
-    } else if (model === 'gateway') {
-      console.log("Step 7. Got gateway:%s's heartbeat:%s with token:%s", json['sid'], json['data'], json['token']);
-    } else {
-      console.log("Step 7. Got %s:%s's heartbeat:%s", json['model'], json['sid'], json['data']);
-    }
-  } else if (cmd === 'write') {
+    updateInterval(json);
+    logDecode(json);
+  }
+  //ici on se sert du logger pour passer des command a la gateway correspondant au bon sid
+  else if (cmd === 'write') {
     // Commands from udpclient.js, pass them to gateway
     var sid = json['sid'];
     if (!sid || !sidToPort[sid] || !sidToAddress[sid]) {
@@ -115,6 +161,8 @@ serverSocket.on('message', function(msg, rinfo){
     }
   } else {
     console.log('recv %s(%d bytes) from client %s:%d\n', msg, msg.length, rinfo.address, rinfo.port);
+    //on l'insere quand meme dans la base de log...
+    log_insert.run(Date.now(),json['sid'], json['model'], json['cmd'], json['data']);
   }
 });
 
@@ -132,14 +180,12 @@ console.log('Demo server, in the following steps:');
 
 serverSocket.bind(serverPort);
 
-function sendWhois() {
-  var cmd = '{"cmd": "whois"}';
-  serverSocket.send(cmd, 0, cmd.length, multicastPort, multicastAddress);
-  console.log('Step 2. Send %s to a multicast address %s:%d.', cmd, multicastAddress, multicastPort);
-}
 
+
+//on demande quel hub sont connecté
 sendWhois();
 
+//on relance un whois toute les 30sec (necessaire pour la detection de fin d'un mouvement)
 setInterval(function() {
   console.log('Step 2. Start another round.');
   sendWhois();
